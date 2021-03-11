95aef51d2c824debb9da7125d17903b781fe6a94
Turner Jr <klt8@psu.edu>
2021-03-11 18:49:33 -0500

    .

--- SampleTest.m
+++ SampleTest.m
@@ -1,6 +1,6 @@
 clear; clc; close all;
 %% load sample data file from 3/10/21
-fileName = 'SampleData.mat';
+fileName = 'SampleData3.mat';
 load(fileName,'-mat');
 thermocoup = SampleData.thermocoupleRespiration;
 opsens = SampleData.opsensRespiration;
@@ -72,11 +72,43 @@ legend([L1,L2],'Thermocouple','OpSens')
 set(gca,'box','off')
 axis tight
 
+%% Spectrogram
+params.tapers = [1,1];
+params.pad = 0;
+params.Fs = fs;
+params.fpass = [0,10];
+params.trialave = 1;
+params.err = [2,0.05];
+movingwin = [1,0.25];
+[filtTherm_S,filtThermo_t,filtThermo_f,~] = mtspecgramc(filtThermocoup,movingwin,params);
+[filtOpSens_S,filtOpSens_t,filtOpsens_f,~] = mtspecgramc(filtOpsens,movingwin,params);
+
+spectrogram = figure('Name','Spectrogram','NumberTitle','off');
+subplot(2,1,1)
+imagesc(filtThermo_t,filtThermo_f,filtTherm_S')
+colormap
+ylabel('Frequency (Hz)')
+xlabel('Time (s)')
+title('Thermocouple Power Spectrum')
+axis xy
+
+subplot(2,1,2)
+imagesc(filtOpSens_t,filtOpsens_f,filtOpSens_S')
+colormap
+ylabel('Frequency (Hz)')
+xlabel('Time (s)')
+title('OpSens Power Spectrum')
+axis xy
+
+
+
+
+%% 
 % mean subtract to remove slow drift
 filtThermocoup = filtThermocoup - mean(filtThermocoup);
  % [time band width, number of tapers]
 tapers_r = [2,3];
-movingwin_r = [3.33,1];
+movingwin_r = [0.5,0.1];
 % Frame rate
 params_r.Fs = fs;
 params_r.fpass = [1,5];
@@ -91,7 +123,7 @@ HR = fr(ridx);   % heart rate, in Hz
 filtOpsens = filtOpsens - mean(filtOpsens);
  % [time band width, number of tapers]
 tapers_r = [2,3];
-movingwin_r = [3.33,1];
+movingwin_r = [0.5,0.1];
 % Frame rate
 params_r.Fs = fs;
 params_r.fpass = [1,5];
@@ -101,3 +133,11 @@ params_r.tapers = tapers_r;
 % largest elements along the frequency direction
 [~,ridx] = max(Sr2,[],2);
 HR_opsens = fr(ridx);   % heart rate, in Hz
+
+figure('Name','Respiration','NumberTitle','off')
+plot(tr,HR,tr2,HR_opsens)
+title('Respiration Rate)')
+ylabel('Frequency(Hz)')
+xlabel('TIme (s)')
+legend('Thermocouple','OpSens Pressure')
+
